1ccbdefc3a5b66baffee2e2fdb5a37f16ce990a4
Stephanie <stephaniecaroline2006@gmail.com>
2025-05-12 13:20:03 -0300

    Implementando o Formulário para Cliente

--- eventos-frontend/src/App.js
+++ eventos-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
 import Home from './views/home/Home';
+import FormCliente from './views/cliente/FormCliente';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
 
     <div className="App">
       
-      <Home />
+      <FormCliente/>
 
       <div style={{marginTop: '6%'}}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
